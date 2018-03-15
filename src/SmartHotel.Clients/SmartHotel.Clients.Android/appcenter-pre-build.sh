dafbc240a59897edcc768a20eebb2857d02e72cc
David Sanchez <dasanagu@microsoft.com>
2018-03-15 10:45:09 -0700

    Changing pathof build scripts in Android

--- src/SmartHotel.Clients/SmartHotel.Clients.Android/appcenter-pre-build.sh
+++ src/SmartHotel.Clients/SmartHotel.Clients.Android/appcenter-pre-build.sh
@@ -1,2 +1,2 @@
 /bin/bash -c nuget restore 'src/SmartHotel.Clients.UITests/SmartHotel.Clients.UITests.csproj' -DisableParallelProcessing
-/Library/Frameworks/Mono.framework/Versions/5_4_1/bin/msbuild /Users/vsts/agent/2.129.1/work/1/s/src/SmartHotel.Clients.UITests/SmartHotel.Clients.UITests.csproj 
+/Library/Frameworks/Mono.framework/Versions/5_4_1/bin/msbuild /Users/vsts/agent/2.129.1/work/1/s/src/SmartHotel.Clients.UITests/SmartHotel.Clients.UITests.csproj /p:Configuration=Release
