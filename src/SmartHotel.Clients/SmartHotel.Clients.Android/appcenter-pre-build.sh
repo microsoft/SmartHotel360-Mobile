7af0cd4ef290ac416c1539af206833783c4204d9
David Sanchez <dasanagu@microsoft.com>
2018-03-15 21:47:01 -0700

    Changing scripts of automated test

--- src/SmartHotel.Clients/SmartHotel.Clients.Android/appcenter-pre-build.sh
+++
@@ -1,2 +0,0 @@
-/bin/bash -c nuget restore 'src/SmartHotel.Clients.UITests/SmartHotel.Clients.UITests.csproj' -DisableParallelProcessing
-/Library/Frameworks/Mono.framework/Versions/5_4_1/bin/msbuild /Users/vsts/agent/2.129.1/work/1/s/src/SmartHotel.Clients.UITests/SmartHotel.Clients.UITests.csproj /p:Configuration=Release
