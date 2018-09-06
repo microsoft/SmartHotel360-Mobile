862ee34036849da0f28fa09a66ba27113a6318df
Piotr Karczmarz <piotrk@interknowlogy.com>
2018-09-06 13:08:39 +0000

    LiveIoTDataService could also returns ambient light status for selected room.

---
+++ src/SmartHotel.Clients/SmartHotel.Clients/Services/IoT/RoomTemperatureBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartHotel.Clients.Core.Services.IoT
+{
+    public abstract class RoomTemperatureBase
+    {
+        protected readonly TemperatureValue Default;
+        
+        protected RoomTemperatureBase(TemperatureValue defaultValue, TemperatureValue minimum, TemperatureValue maximum)
+        {
+            Default = defaultValue;
+            Minimum = minimum;
+            Maximum = maximum;
+
+            Value = Default;
+        }
+
+        public TemperatureValue Value { get; protected set; }
+
+        public TemperatureValue Minimum { get; protected set; }
+        public TemperatureValue Maximum { get; protected set; }
+    }
+}
