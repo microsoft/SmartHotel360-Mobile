0609906a60f1090e28d5805716ed55dc5a33335b
Piotr Karczmarz <piotrk@interknowlogy.com>
2018-09-06 14:27:09 +0200

    Added LiveIoTDataService that will act as source data and have access to all available IoT room devices (temperature/ambient light sensors).

---
+++ src/SmartHotel.Clients/SmartHotel.Clients/Services/IoT/TemperatureExtesions.cs
@@ -0,0 +1,23 @@
+﻿namespace SmartHotel.Clients.Core.Services.IoT
+{
+    public static class TemperatureExtesions
+    {
+        public static TemperatureValue ToFahrenheit(this TemperatureValue temp)
+        {
+            if (temp.Unit == TemperatureTypes.Fahrenheit)
+                return temp;
+
+            var fahr = 9.0 / 5.0 * temp.Value + 32;
+            return new TemperatureValue((float)fahr, TemperatureTypes.Fahrenheit);
+        }
+
+        public static TemperatureValue ToCelsius(this TemperatureValue temp)
+        {
+            if (temp.Unit == TemperatureTypes.Celsius)
+                return temp;
+
+            var celc = (temp.Value - 32) * 5.0 / 9.0;
+            return new TemperatureValue((float)celc, TemperatureTypes.Celsius);
+        }
+    }
+}
