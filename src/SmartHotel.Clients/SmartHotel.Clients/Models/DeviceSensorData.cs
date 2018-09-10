e573672310a0201a0787a8a8eea360a893a0ec55
Travis Schilling <traviss@interknowlogy.com>
2018-09-10 16:15:37 -0700

    Adding DeviceSensorData that matches the model being returned by the RoomDevices API

---
+++ src/SmartHotel.Clients/SmartHotel.Clients/Models/DeviceSensorData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHotel.Clients.Core.Models
+{
+	[DataContract(Name = "deviceSensorData")]
+    public class DeviceSensorData
+    {
+	    [DataMember(Name = "id")]
+	    public string Id { get; set; }
+	    [DataMember(Name = "sensorId")]
+	    public string SensorId { get; set; }
+	    [DataMember(Name = "roomId")]
+	    public string RoomId { get; set; }
+	    [DataMember(Name = "sensorReading")]
+	    public string SensorReading { get; set; }
+	    [DataMember(Name = "sensorDataType")]
+	    public string SensorDataType { get; set; }
+	    [DataMember(Name = "DesiredValue")]
+	    public string DesiredValue { get; set; }
+	    [DataMember(Name = "EventTimestamp")]
+	    public DateTime EventTimestamp { get; set; }
+    }
+}
