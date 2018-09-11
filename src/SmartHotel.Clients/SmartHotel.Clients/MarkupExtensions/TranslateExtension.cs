71a0861472fd2418c728950b5a1fccf284b497c1
Travis Schilling <traviss@interknowlogy.com>
2018-09-11 09:36:30 -0700

    Updated the TranslateExtension to have support for a format string

--- src/SmartHotel.Clients/SmartHotel.Clients/MarkupExtensions/TranslateExtension.cs
+++ src/SmartHotel.Clients/SmartHotel.Clients/MarkupExtensions/TranslateExtension.cs
@@ -7,21 +7,29 @@ using Xamarin.Forms.Xaml;
 
 namespace SmartHotel.Clients.Core.MarkupExtensions
 {
-    [ContentProperty("Text")]
-    public class TranslateExtension : IMarkupExtension
-    {
-        public string Text { get; set; }
+	[ContentProperty( "Text" )]
+	public class TranslateExtension : IMarkupExtension
+	{
+		public string Text { get; set; }
+		public string StringFormat { get; set; }
 
-        public object ProvideValue(IServiceProvider serviceProvider)
-        {
-            if (Text == null)
-                return null;
+		public object ProvideValue( IServiceProvider serviceProvider )
+		{
+			if ( Text == null )
+				return null;
 
-            var assembly = typeof(TranslateExtension).GetTypeInfo().Assembly;
-            var assemblyName = assembly.GetName();
-            ResourceManager resourceManager = new ResourceManager($"{assemblyName.Name}.Resources", assembly);
+			var assembly = typeof( TranslateExtension ).GetTypeInfo().Assembly;
+			var assemblyName = assembly.GetName();
+			ResourceManager resourceManager = new ResourceManager( $"{assemblyName.Name}.Resources", assembly );
 
-            return resourceManager.GetString(Text, CultureInfo.CurrentCulture);
-        }
-    }
+			string result = resourceManager.GetString( Text, CultureInfo.CurrentCulture );
+			if ( !string.IsNullOrWhiteSpace( StringFormat ) )
+			{
+				result = string.Format( StringFormat, result );
+			}
+
+			return result;
+
+		}
+	}
 }
