PiotrKarczmarz/SmartHotel360-mobile-desktop-apps

updated temp and light sliders to be disabled when the RoomDevicesDataService is Using Fakes

src/SmartHotel.Clients/SmartHotel.Clients/ViewModels/MyRoomViewModel.cs
@@ -49,6 +49,8 @@ namespace SmartHotel.Clients.Core.ViewModels
 			SetNeed();
 		}
 
+		public bool UseRealRoomDevices => !_roomDevicesDataService.UseFakes;
+
 		public double CurrentAmbientLight
 		{
 			get => _currentAmbientLight;
@@ -237,8 +239,8 @@ namespace SmartHotel.Clients.Core.ViewModels
 			TemperatureMinimum = roomTemperature.Minimum.RawValue;
 			CurrentTemperature = roomTemperature.Value.RawValue;
 			DesiredTemperature = roomTemperature.Desired.RawValue;
-			
-			
+
+
 
 			RoomAmbientLight roomAmbientLight = roomAmbientLightTask.Result;
 			AmbientLightMaximum = roomAmbientLight.Maximum.RawValue;

microsoft/SmartHotel360-Mobile

src/SmartHotel.Clients/SmartHotel.Clients/ViewModels/MyRoomViewModel.cs
@@ -47,6 +47,8 @@ namespace SmartHotel.Clients.Core.ViewModels
 			SetNeed();
 		}
 
+		public bool UseRealRoomDevices => !roomDevicesDataService.UseFakes;
+
 	    public double CurrentAmbientLight
 	    {
 	        get => currentAmbientLight;
@@ -181,8 +183,8 @@ namespace SmartHotel.Clients.Core.ViewModels
 			TemperatureMinimum = roomTemperature.Minimum.RawValue;
 			CurrentTemperature = roomTemperature.Value.RawValue;
 			DesiredTemperature = roomTemperature.Desired.RawValue;
-			
-			
+
+
 
 			RoomAmbientLight roomAmbientLight = roomAmbientLightTask.Result;
 			AmbientLightMaximum = roomAmbientLight.Maximum.RawValue;