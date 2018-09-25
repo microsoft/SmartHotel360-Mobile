PiotrKarczmarz/SmartHotel360-mobile-desktop-apps

Updating AppSettings, SettingsService, and RemoteSettings to include a RoomId

src/SmartHotel.Clients/SmartHotel.Clients/AppSettings.cs
@@ -5,7 +5,7 @@ using SmartHotel.Clients.Core.Models;
 
 namespace SmartHotel.Clients.Core
 {
-    public static class AppSettings
+	public static class AppSettings
     {
         //IF YOU DEPLOY YOUR OWN ENDPOINT REPLACE THE VALUEW BELOW
         //App Center
@@ -38,6 +38,9 @@ namespace SmartHotel.Clients.Core
 
         // Booking 
         private const bool DefaultHasBooking = false;
+
+		// Room Devices
+	    private const string DefaultRoomId = "";
 
         // Fakes
         private const bool DefaultUseFakes = false;
@@ -193,5 +196,12 @@ namespace SmartHotel.Clients.Core
         {
             Settings.Remove(nameof(User));
         }
+
+	    public static string RoomId
+	    {
+		    get => Settings.GetValueOrDefault(nameof(RoomId), DefaultRoomId);
+
+		    set => Settings.AddOrUpdateValue(nameof(RoomId), value);
+	    }
     }
 }

microsoft/SmartHotel360-Mobile

src/SmartHotel.Clients/SmartHotel.Clients/AppSettings.cs
@@ -4,7 +4,7 @@ using SmartHotel.Clients.Core.Utils;
 
 namespace SmartHotel.Clients.Core
 {
-    public static class AppSettings
+	public static class AppSettings
     {
         //IF YOU DEPLOY YOUR OWN ENDPOINT REPLACE THE VALUEW BELOW
 
@@ -38,6 +38,9 @@ namespace SmartHotel.Clients.Core
 
         // Booking 
         const bool defaultHasBooking = false;
+
+		// Room Devices
+	    private const string defaultRoomId = "";
 
         // Fakes
         const bool defaultUseFakes = false;
@@ -171,5 +174,11 @@ namespace SmartHotel.Clients.Core
         }
 
         public static void RemoveUserData() => Preferences.Remove(nameof(User));
+		
+		public static string RoomId
+	    {
+		    get => Preferences.Get(nameof(RoomId), defaultRoomId);
+		    set => Preferences.Set(nameof(RoomId), value);
+	    }
     }
 }