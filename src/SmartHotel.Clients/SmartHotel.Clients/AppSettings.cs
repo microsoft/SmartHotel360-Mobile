PiotrKarczmarz/SmartHotel360-mobile-desktop-apps

Introduced DefaultRoomDevicesBaseUri. LiveIoTDataService is initalized using IoC container via Locator.

src/SmartHotel.Clients/SmartHotel.Clients/AppSettings.cs
@@ -20,6 +20,7 @@ namespace SmartHotel.Clients.Core
         private const string DefaultNotificationsEndpoint = "http://sh360services-public.eastus2.cloudapp.azure.com/notifications-api";
         private const string DefaultSettingsFileUrl = "http://sh360services-public.eastus2.cloudapp.azure.com/configuration-api/cfg/public-http";
         private const string DefaultImagesBaseUri = "http://sh360imgpublic.blob.core.windows.net";
+        private const string DefaultRoomDevicesBaseUri = "";
 
         // Maps
         private const string DefaultBingMapsApiKey = "9D6ZuqeGpcfZ9PVYR1BQ~ofsY_N_KDywcNM-Y0Io5aA~AvqaBtSnHxFfX7flAqux2Q6eYSIreLwDxnswabgPlEOXmoEXXt6u1O6In0hqICy8";
@@ -100,6 +101,13 @@ namespace SmartHotel.Clients.Core
             get => Settings.GetValueOrDefault(nameof(ImagesBaseUri), DefaultImagesBaseUri);
 
             set => Settings.AddOrUpdateValue(nameof(ImagesBaseUri), value);
+        }
+
+        public static string RoomDevicesBaseUri
+        {
+            get => Settings.GetValueOrDefault(nameof(RoomDevicesBaseUri), DefaultRoomDevicesBaseUri);
+
+            set => Settings.AddOrUpdateValue(nameof(RoomDevicesBaseUri), value);
         }
 
         public static string SkypeBotId

src/SmartHotel.Clients/SmartHotel.Clients/ViewModels/Base/Locator.cs
@@ -14,6 +14,7 @@ using SmartHotel.Clients.Core.Services.Request;
 using SmartHotel.Clients.Core.Services.Settings;
 using SmartHotel.Clients.Core.Services.Suggestion;
 using SmartHotel.Clients.Core.Models;
+using SmartHotel.Clients.Core.Services.IoT;
 
 namespace SmartHotel.Clients.Core.ViewModels.Base
 {
@@ -45,6 +46,7 @@ namespace SmartHotel.Clients.Core.ViewModels.Base
             _containerBuilder.RegisterType<OpenUriService>().As<IOpenUriService>();
             _containerBuilder.RegisterType<RequestService>().As<IRequestService>();
             _containerBuilder.RegisterType<DefaultBrowserCookiesService>().As<IBrowserCookiesService>();
+            _containerBuilder.RegisterType<LiveIoTDataService>().As<ILiveIoTDataService>();
             _containerBuilder.RegisterType<GravatarUrlProvider>().As<IAvatarUrlProvider>();
             _containerBuilder.RegisterType(typeof(SettingsService)).As(typeof(ISettingsService<RemoteSettings>));
 

microsoft/SmartHotel360-Mobile

src/SmartHotel.Clients/SmartHotel.Clients/AppSettings.cs
@@ -21,6 +21,7 @@ namespace SmartHotel.Clients.Core
 
         // Endpoints
         const string defaultImagesBaseUri = "https://sh360publicimg.blob.core.windows.net";
+        const string defaultRoomDevicesBaseUri = "";
 
         // Maps
         const string defaultBingMapsApiKey = "9D6ZuqeGpcfZ9PVYR1BQ~ofsY_N_KDywcNM-Y0Io5aA~AvqaBtSnHxFfX7flAqux2Q6eYSIreLwDxnswabgPlEOXmoEXXt6u1O6In0hqICy8";
@@ -99,6 +100,12 @@ namespace SmartHotel.Clients.Core
         {
             get => Preferences.Get(nameof(ImagesBaseUri), defaultImagesBaseUri);
             set => Preferences.Set(nameof(ImagesBaseUri), value);
+        }
+
+        public static string RoomDevicesBaseUri
+        {
+            get => Preferences.Get(nameof(RoomDevicesBaseUri), defaultRoomDevicesBaseUri);
+            set => Preferences.Set(nameof(RoomDevicesBaseUri), value);
         }
 
         public static string SkypeBotId

src/SmartHotel.Clients/SmartHotel.Clients/ViewModels/Base/Locator.cs
@@ -15,6 +15,7 @@ using SmartHotel.Clients.Core.Services.Suggestion;
 using SmartHotel.Clients.Core.Models;
 using SmartHotel.Clients.Core.Services.File;
 using SmartHotel.Clients.Core.Services.Geolocator;
+using SmartHotel.Clients.Core.Services.IoT;
 
 namespace SmartHotel.Clients.Core.ViewModels.Base
 {
@@ -38,6 +39,7 @@ namespace SmartHotel.Clients.Core.ViewModels.Base
             containerBuilder.RegisterType<OpenUriService>().As<IOpenUriService>();
             containerBuilder.RegisterType<RequestService>().As<IRequestService>();
             containerBuilder.RegisterType<DefaultBrowserCookiesService>().As<IBrowserCookiesService>();
+            containerBuilder.RegisterType<LiveIoTDataService>().As<ILiveIoTDataService>();
             containerBuilder.RegisterType<GravatarUrlProvider>().As<IAvatarUrlProvider>();
             containerBuilder.RegisterType<FileService>().As<IFileService>();
             containerBuilder.RegisterType(typeof(SettingsService)).As(typeof(ISettingsService<RemoteSettings>));