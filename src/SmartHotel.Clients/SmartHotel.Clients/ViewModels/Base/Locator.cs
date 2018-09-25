PiotrKarczmarz/SmartHotel360-mobile-desktop-apps

IRoomDevicesDataService is created as single instance.

src/SmartHotel.Clients/SmartHotel.Clients/ViewModels/Base/Locator.cs
@@ -46,7 +46,7 @@ namespace SmartHotel.Clients.Core.ViewModels.Base
             _containerBuilder.RegisterType<OpenUriService>().As<IOpenUriService>();
             _containerBuilder.RegisterType<RequestService>().As<IRequestService>();
             _containerBuilder.RegisterType<DefaultBrowserCookiesService>().As<IBrowserCookiesService>();
-            _containerBuilder.RegisterType<RoomDevicesDataService>().As<IRoomDevicesDataService>();
+            _containerBuilder.RegisterType<RoomDevicesDataService>().As<IRoomDevicesDataService>().SingleInstance();
             _containerBuilder.RegisterType<GravatarUrlProvider>().As<IAvatarUrlProvider>();
             _containerBuilder.RegisterType(typeof(SettingsService)).As(typeof(ISettingsService<RemoteSettings>));
 

microsoft/SmartHotel360-Mobile

src/SmartHotel.Clients/SmartHotel.Clients/ViewModels/Base/Locator.cs
@@ -39,7 +39,7 @@ namespace SmartHotel.Clients.Core.ViewModels.Base
             containerBuilder.RegisterType<OpenUriService>().As<IOpenUriService>();
             containerBuilder.RegisterType<RequestService>().As<IRequestService>();
             containerBuilder.RegisterType<DefaultBrowserCookiesService>().As<IBrowserCookiesService>();
-            containerBuilder.RegisterType<RoomDevicesDataService>().As<IRoomDevicesDataService>();
+            containerBuilder.RegisterType<RoomDevicesDataService>().As<IRoomDevicesDataService>().SingleInstance();
             containerBuilder.RegisterType<GravatarUrlProvider>().As<IAvatarUrlProvider>();
             containerBuilder.RegisterType<FileService>().As<IFileService>();
             containerBuilder.RegisterType(typeof(SettingsService)).As(typeof(ISettingsService<RemoteSettings>));